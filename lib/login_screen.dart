41fbbfa2a6f946a0db0eb60b3d1ff537244381d0
MarianNULP <marian.pavlos.ki.2021@lpnu.ua>
2024-10-13 18:10:58 +0300

    Додавання фічі

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,19 +11,58 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   bool _isPasswordVisible = false;
 
+  String capitalizeFirstLetter(String input) {
+    if (input.isEmpty) return input;
+    return input[1].toUpperCase() + input.substring(2);
+  }
+
   void _login() {
     final email = _emailController.text;
     final password = _passwordController.text;
+    const emailPattern = r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$';
+    final emailRegExp = RegExp(emailPattern);
 
     if (_formKey.currentState!.validate()) {
-      if (email == "test@example.com" && password == "password") {
+      // Додаткова перевірка для формату електронної пошти
+      if (!emailRegExp.hasMatch(email)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Please enter a valid email address"),
+            backgroundColor: Colors.orange,
+          ),
+        );
+        return;
+      }
+
+      // Перевірка логіну та паролю
+      if (password == "password") {
+        String userName = email.split('@')[0]; // Отримуємо ім'я до @
+        capitalizeFirstLetter(userName);
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text("Login successful!"),
             backgroundColor: Colors.green,
           ),
         );
-        // Тут більше немає переходу на UserMenu
+
+        // Показуємо діалог з привітанням
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Welcome"),
+              content: Text("Welcome $userName!"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                ),
+              ],
+            );
+          },
+        );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -34,6 +73,7 @@ class _LoginScreenState extends State<LoginScreen> {
       }
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
